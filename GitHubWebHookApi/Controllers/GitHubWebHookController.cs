f352ebfb30c3a36479e6d8ca5bad95e3fed7f3b1
Vikram shah <vshah@tavisca.com>
2019-10-08 00:40:19 +0530

    Implemented post Request for webhook

---
+++ GitHubWebHookApi/Controllers/GitHubWebHookController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using GitHubWebHookApi.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using GitHubWebHookApi.Models.MYDelegate;
+
+namespace GitHubWebHookApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GitHubWebHookController : ControllerBase
+    {
+        // GET: api/GitHubWebHook
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/GitHubWebHook/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/GitHubWebHook
+        [HttpPost]
+        public void Post(GitHubPayLoad payload)
+        {
+            string pullCommentUrl = payload.pull_request.review_comments_url;
+            ProcessWebHookPayloadAsync(pullCommentUrl).Wait();
+            
+        }
+
+        private async Task ProcessWebHookPayloadAsync(string pullCommentUrl)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri("https://api.github.com");
+                var token = "ea1ce4f2d92e3bc15ecfef2088f181cf7e8b1a5e";
+                client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("AppName", "1.0"));
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token", token);
+
+                var response = await client.GetAsync("repos/tavisca-vshah/hello/pulls/1/comments");
+                string data = "";
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    var comments = JsonConvert.DeserializeObject<IEnumerable<Comment>>(responseString).ToList<Comment>();
+                    comments.ForEach(c => data += c.user.login + "\n" + c.body + "\n");
+                    data += $"Total Comments: {comments.Count}\n\n";
+                }
+                else
+                {
+                    data = $"Error In Fetching Data fro api call {response.ReasonPhrase}";
+                    data += $"Total Comments: 0\n\n";
+                }
+
+                System.IO.File.WriteAllText(@"C:\Users\vshah\source\repos\GitHubWebHookApi\GitHubWebHookApi\response.txt", data);
+            }
+        }
+
+        // PUT: api/GitHubWebHook/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ GitHubWebHookApi/Models/Comment.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace GitHubWebHookApi.Models
+{
+    namespace MYDelegate
+    {
+        public class User
+        {
+            public string login { get; set; }
+            public int id { get; set; }
+            public string node_id { get; set; }
+            public string avatar_url { get; set; }
+            public string gravatar_id { get; set; }
+            public string url { get; set; }
+            public string html_url { get; set; }
+            public string followers_url { get; set; }
+            public string following_url { get; set; }
+            public string gists_url { get; set; }
+            public string starred_url { get; set; }
+            public string subscriptions_url { get; set; }
+            public string organizations_url { get; set; }
+            public string repos_url { get; set; }
+            public string events_url { get; set; }
+            public string received_events_url { get; set; }
+            public string type { get; set; }
+            public bool site_admin { get; set; }
+        }
+
+        public class Self
+        {
+            public string href { get; set; }
+        }
+
+        public class Html
+        {
+            public string href { get; set; }
+        }
+
+        public class PullRequest
+        {
+            public string href { get; set; }
+        }
+
+        public class Links
+        {
+            public Self self { get; set; }
+            public Html html { get; set; }
+            public PullRequest pull_request { get; set; }
+        }
+
+        public class Comment
+        {
+            public string url { get; set; }
+            public int pull_request_review_id { get; set; }
+            public int id { get; set; }
+            public string node_id { get; set; }
+            public string diff_hunk { get; set; }
+            public string path { get; set; }
+            public int position { get; set; }
+            public int original_position { get; set; }
+            public string commit_id { get; set; }
+            public string original_commit_id { get; set; }
+            public User user { get; set; }
+            public string body { get; set; }
+            public DateTime created_at { get; set; }
+            public DateTime updated_at { get; set; }
+            public string html_url { get; set; }
+            public string pull_request_url { get; set; }
+            public string author_association { get; set; }
+            public Links _links { get; set; }
+        }
+    }
+
+}
